16abeb6dbd8c149692c4628337b49fb2ac6c87dc
Ori Molad <orimolad17@gmail.com>
2020-11-04 20:54:45 -0800

    fixed the map to change , now just the circles to change colors properly

--- src/Map.js
+++ src/Map.js
@@ -1,16 +1,26 @@
-import React from 'react';
+import React, { Fragment, useEffect } from 'react';
 import './Map.css';
-import { MapContainer as LeafletMap, TileLayer } from "react-leaflet";
+import { MapContainer as LeafletMap, TileLayer, useMap } from "react-leaflet";
 import { showDataOnMap } from './Util';
 
-function Map({countries, casesType, center, zoom}) {
+function UpdateMapCenter({ center }) {
+    const map = useMap();
+    useEffect(() => {
+        map.flyTo(center)
+    }, [center])
+    return (<Fragment />)
+}
+
+function Map({ countries, casesType, center, zoom }) {
     return (
         <div className="map">
-            <LeafletMap center={center} zoom={zoom}>
+            <LeafletMap center={center} zoom={zoom} >
                 <TileLayer
-                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" 
+                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                     attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+
                 />
+                <UpdateMapCenter center={center} />
                 {showDataOnMap(countries, casesType)}
             </LeafletMap>
         </div>
