fd69f81c92c2fc4188157abdb718b28babea33f8
Ori Molad <orimolad17@gmail.com>
2020-11-11 14:20:32 -0800

    fixed bugs, need to set up firebase

--- src/Map.js
+++ src/Map.js
@@ -3,11 +3,11 @@ import './Map.css';
 import { MapContainer as LeafletMap, TileLayer, useMap } from "react-leaflet";
 import { showDataOnMap } from './Util';
 
-function UpdateMapCenter({ center }) {
+function UpdateMapCenter({ center, zoom }) {
     const map = useMap();
     useEffect(() => {
-        map.flyTo(center)
-    }, [center])
+        map.flyTo(center, zoom)
+    }, [center, zoom])
     return (<Fragment />)
 }
 
@@ -21,7 +21,7 @@ function Map({ countries, casesType, center, zoom }) {
                     attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
 
                 />
-                <UpdateMapCenter center={center} />
+                <UpdateMapCenter center={center} zoom={zoom}/>
                 {showDataOnMap(countries, casesType)}
             </LeafletMap>
         </div>
